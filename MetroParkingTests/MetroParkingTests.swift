753da5bf176f785978cb19744de8a6e258e15688
Tom Kwok <junhaotom@gmail.com>
2025-05-18 21:12:50 +1000

    Initial Commit

---
+++ MetroParkingTests/MetroParkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetroParkingTests.swift
+//  MetroParkingTests
+//
+//  Created by Tom Kwok on 18/5/2025.
+//
+
+import Testing
+@testable import MetroParking
+
+struct MetroParkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

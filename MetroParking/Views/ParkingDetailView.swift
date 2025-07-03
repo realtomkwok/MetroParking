acd8637c6466c1121ddfc4f56d033acda07a4cc1
Tom Kwok <junhaotom@gmail.com>
2025-07-03 21:26:50 +1000

    Refactor the container for each info into a reusable component `InfoCard()`

--- MetroParking/Views/ParkingDetailView.swift
+++ MetroParking/Views/ParkingDetailView.swift
@@ -47,250 +47,130 @@ struct FacilityDetailView: View {
             .padding()
 
             Grid {
-              GridRow {
-                // TODO: Current Stauts
+				GridRow {
+					InfoCard(headingIcon: "gauge", headingText: "Capacity"){
+							/// Current available spaces
+							// TODO: Dynamic position of the number
+						VStack(alignment: .trailing) {
+							VStack(alignment: .trailing, spacing: 0) {
+								Text(
+									"\(facility.displayAvailableSpots)"
+								)
+								.font(.system(size: 48))
+								.fontDesign(.rounded)
+								.foregroundStyle(.primary)
+								.contentTransition(
+									.numericText(
+										value: Double(
+											facility
+												.currentAvailableSpots
+										)
+									)
+								)
+								Text("spaces")
+									.foregroundStyle(.secondary)
+							}
+							
+							Gauge(value: occupancyProgress) {
+								Label("Value", systemImage: "car")
+							}
+							.gaugeStyle(.accessoryLinear)
+							.tint(
+								Gradient(colors: [
+									AvailabilityStatus.available
+										.color,
+									AvailabilityStatus.almostFull
+										.color,
+									AvailabilityStatus.full.color,
+								])
+							)
+						}
+						.padding()
+					}
+					InfoCard(headingIcon: "gauge", headingText: "Capacity") {
+							/// Current available spaces
+							// TODO: Dynamic position of the number
+						VStack(alignment: .trailing) {
+							VStack(alignment: .trailing, spacing: 0) {
+								Text(
+									"\(facility.displayAvailableSpots)"
+								)
+								.font(.system(size: 48))
+								.fontDesign(.rounded)
+								.foregroundStyle(.primary)
+								.contentTransition(
+									.numericText(
+										value: Double(
+											facility
+												.currentAvailableSpots
+										)
+									)
+								)
+								Text("spaces")
+									.foregroundStyle(.secondary)
+							}
+							
+							Gauge(value: occupancyProgress) {
+								Label("Value", systemImage: "car")
+							}
+							.gaugeStyle(.accessoryLinear)
+							.tint(
+								Gradient(colors: [
+									AvailabilityStatus.available
+										.color,
+									AvailabilityStatus.almostFull
+										.color,
+									AvailabilityStatus.full.color,
+								])
+							)
+						}
+						.padding()
+					}
 
-                VStack(alignment: .leading) {
-                  HStack(alignment: .center) {
-                    Label("Capacity", systemImage: "gauge")
-                      .font(.headline)
-                      .textCase(.uppercase)
-                      .foregroundStyle(.secondary)
-                  }
-                  .padding()
-
-                  Spacer()
-
-                  /// Current available spaces
-                  /// TODO: Dynamic position of the number
-                  VStack(alignment: .trailing) {
-                    VStack(alignment: .trailing, spacing: 0) {
-                      Text(
-                        "\(facility.displayAvailableSpots)"
-                      )
-                      .font(.system(size: 48))
-                      .fontDesign(.rounded)
-                      .foregroundStyle(.primary)
-                      .contentTransition(
-                        .numericText(
-                          value: Double(
-                            facility
-                              .currentAvailableSpots
-                          )
-                        )
-                      )
-                      Text("spaces")
-                        .foregroundStyle(.secondary)
-                    }
-
-                    Gauge(value: occupancyProgress) {
-                      Label("Value", systemImage: "car")
-                    }
-                    .gaugeStyle(.accessoryLinear)
-                    .tint(
-                      Gradient(colors: [
-                        AvailabilityStatus.available
-                          .color,
-                        AvailabilityStatus.almostFull
-                          .color,
-                        AvailabilityStatus.full.color,
-                      ])
-                    )
-                  }
-                  .padding()
-
-                }
-                .frame(height: 200)
-                .frame(maxWidth: .infinity)
-                .background(.thickMaterial)
-                .clipShape(
-                  RoundedRectangle(
-                    cornerRadius: 24,
-                    style: .continuous
-                  )
-                )
-
-                VStack(alignment: .leading) {
-                  HStack(alignment: .center) {
-                    Label("Capacity", systemImage: "gauge")
-                      .font(.headline)
-                      .textCase(.uppercase)
-                      .foregroundStyle(.secondary)
-                  }
-                  .padding()
-
-                  Spacer()
-
-                  /// Current available spaces
-                  /// TODO: Dynamic position of the number
-                  VStack(alignment: .trailing) {
-                    VStack(alignment: .trailing, spacing: 0) {
-                      Text(
-                        "\(facility.displayAvailableSpots)"
-                      )
-                      .font(.system(size: 48))
-                      .fontDesign(.rounded)
-                      .foregroundStyle(.primary)
-                      .contentTransition(
-                        .numericText(
-                          value: Double(
-                            facility
-                              .currentAvailableSpots
-                          )
-                        )
-                      )
-                      Text("spaces")
-                        .foregroundStyle(.secondary)
-                    }
-
-                    Gauge(value: occupancyProgress) {
-                      Label("Value", systemImage: "car")
-                    }
-                    .gaugeStyle(.accessoryLinear)
-                    .tint(
-                      Gradient(colors: [
-                        AvailabilityStatus.available
-                          .color,
-                        AvailabilityStatus.almostFull
-                          .color,
-                        AvailabilityStatus.full.color,
-                      ])
-                    )
-                  }
-                  .padding()
-
-                }
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                .background(.thickMaterial)
-                .clipShape(
-                  RoundedRectangle(
-                    cornerRadius: 24,
-                    style: .continuous
-                  )
-                )
-
-              }
+				}
 
               // TODO: Forecast and trend
               GridRow {
-                VStack(alignment: .leading) {
-                  HStack(alignment: .center) {
-                    Label("Capacity", systemImage: "gauge")
-                      .font(.headline)
-                      .textCase(.uppercase)
-                      .foregroundStyle(.secondary)
-                  }
-                  .padding()
-
-                  Spacer()
-
-                  /// Current available spaces
-                  /// TODO: Dynamic position of the number
-                  VStack(alignment: .trailing) {
-                    VStack(alignment: .trailing, spacing: 0) {
-                      Text(
-                        "\(facility.displayAvailableSpots)"
-                      )
-                      .font(.system(size: 48))
-                      .fontDesign(.rounded)
-                      .foregroundStyle(.primary)
-                      .contentTransition(
-                        .numericText(
-                          value: Double(
-                            facility
-                              .currentAvailableSpots
-                          )
-                        )
-                      )
-                      Text("spaces")
-                        .foregroundStyle(.secondary)
-                    }
-
-                    Gauge(value: occupancyProgress) {
-                      Label("Value", systemImage: "car")
-                    }
-                    .gaugeStyle(.accessoryLinear)
-                    .tint(
-                      Gradient(colors: [
-                        AvailabilityStatus.available
-                          .color,
-                        AvailabilityStatus.almostFull
-                          .color,
-                        AvailabilityStatus.full.color,
-                      ])
-                    )
-                  }
-                  .padding()
-
-                }
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                .background(.thickMaterial)
-                .clipShape(
-                  RoundedRectangle(
-                    cornerRadius: 24,
-                    style: .continuous
-                  )
-                )
-
-                VStack(alignment: .leading) {
-                  HStack(alignment: .center) {
-                    Label("Capacity", systemImage: "gauge")
-                      .font(.headline)
-                      .textCase(.uppercase)
-                      .foregroundStyle(.secondary)
-                  }
-                  .padding()
-
-                  Spacer()
-
-                  /// Current available spaces
-                  /// TODO: Dynamic position of the number
-                  VStack(alignment: .trailing) {
-                    VStack(alignment: .trailing, spacing: 0) {
-                      Text(
-                        "\(facility.displayAvailableSpots)"
-                      )
-                      .font(.system(size: 48))
-                      .fontDesign(.rounded)
-                      .foregroundStyle(.primary)
-                      .contentTransition(
-                        .numericText(
-                          value: Double(
-                            facility
-                              .currentAvailableSpots
-                          )
-                        )
-                      )
-                      Text("spaces")
-                        .foregroundStyle(.secondary)
-                    }
-
-                    Gauge(value: occupancyProgress) {
-                      Label("Value", systemImage: "car")
-                    }
-                    .gaugeStyle(.accessoryLinear)
-                    .tint(
-                      Gradient(colors: [
-                        AvailabilityStatus.available
-                          .color,
-                        AvailabilityStatus.almostFull
-                          .color,
-                        AvailabilityStatus.full.color,
-                      ])
-                    )
-                  }
-                  .padding()
-
-                }
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                .background(.thickMaterial)
-                .clipShape(
-                  RoundedRectangle(
-                    cornerRadius: 24,
-                    style: .continuous
-                  )
-                )
-
+				  InfoCard(headingIcon: "gauge", headingText: "Capacity") {
+						  /// Current available spaces
+						  // TODO: Dynamic position of the number
+					  VStack(alignment: .trailing) {
+						  VStack(alignment: .trailing, spacing: 0) {
+							  Text(
+								"\(facility.displayAvailableSpots)"
+							  )
+							  .font(.system(size: 48))
+							  .fontDesign(.rounded)
+							  .foregroundStyle(.primary)
+							  .contentTransition(
+								.numericText(
+									value: Double(
+										facility
+											.currentAvailableSpots
+									)
+								)
+							  )
+							  Text("spaces")
+								  .foregroundStyle(.secondary)
+						  }
+						  
+						  Gauge(value: occupancyProgress) {
+							  Label("Value", systemImage: "car")
+						  }
+						  .gaugeStyle(.accessoryLinear)
+						  .tint(
+							Gradient(colors: [
+								AvailabilityStatus.available
+									.color,
+								AvailabilityStatus.almostFull
+									.color,
+								AvailabilityStatus.full.color,
+							])
+						  )
+					  }
+					  .padding()
+				  }
+	
               }
 
               // TODO: Look around
@@ -357,8 +237,48 @@ struct FacilityDetailView: View {
 
 }
 
-extension FacilityDetailView {
+struct InfoCard<Content: View>: View {
+	let headingIcon: String
+	let headingText: String
+	
+	private let content: () -> Content
+	
+	init(
+		headingIcon: String,
+		headingText: String,
+		@ViewBuilder content: @escaping () -> Content
+	) {
+		self.headingIcon = headingIcon
+		self.headingText = headingText
+		self.content = content
+	}
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			HStack(alignment: .center) {
+				Label(headingText, systemImage: headingIcon)
+					.font(.caption)
+					.fontWeight(.semibold)
+					.tracking(0.4)
+					.textCase(.uppercase)
+					.foregroundStyle(.secondary)
+			}
+			.padding()
+			
+			Spacer()
+			
+			content()
 
+		}
+		.frame(maxWidth: .infinity, maxHeight: 200)
+		.background(.thickMaterial)
+		.clipShape(
+			RoundedRectangle(
+				cornerRadius: 24,
+				style: .continuous
+			)
+		)
+	}
 }
 
 #Preview {

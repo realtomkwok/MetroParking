53cd2fe25e7bb254c8b7dbbeabcc3dd922684841
Tom Kwok <junhaotom@gmail.com>
2025-12-07 16:37:44 +1100

    Refactor `BackgroundGradient` for improved color handling, dynamic animations, and environment-aware theming.

--- MetroParking/BackgroundGradient.swift
+++ MetroParking/BackgroundGradient.swift
@@ -11,32 +11,45 @@ import SwiftUI
 struct BackgroundGradient: View {
 	@State private var isAnimating = false
 
+	@Environment(\.colorScheme) private var scheme
+
+	var width: Int = 3
+	var height: Int = 3
+	var points: [SIMD2<Float>] {
+		return [
+			[0.0, 0.0],
+			[isAnimating ? 0.4 : 0.9, 0.0],
+			[1.0, 0.0],
+			[0.0, isAnimating ? 0.2 : 0.3],
+			[0.8, isAnimating ? 0.2 : 0.1],
+			[1.0, isAnimating ? 0.1 : 0.2],
+			[0.0, 1.2],
+			[0.5, 1.0],
+			[1.0, 1.0],
+		]
+	}
+
+	var colors: [Color] {
+
+		return [
+			scheme == .light ? .cyan : .cyan.mix(with: .black, by: 0.6),
+			scheme == .light ? .white.opacity(1.0) :	.white.opacity(0.3),
+			scheme == .light ? .white.opacity(0.8) : .white.opacity(0.3),
+			.cyan.opacity(0.1),
+			.cyan.opacity(0.1),
+			.cyan.opacity(0.1),
+			.clear,
+			.clear,
+			.clear,
+		]
+	}
+
 	var body: some View {
 		MeshGradient(
-			width: 3,
-			height: 3,
-			points: [
-				[0.0, 0.0],
-				[0.5, 0.0],
-				[1.0, 0.0],
-				[0.0, 0.2],
-				[isAnimating ? 0.9 : 0.3, isAnimating ? 0.6 : 0.2],
-				[1.0, isAnimating ? 0.2 : 0.6],
-				[0.0, 1.0],
-				[0.5, 1.0],
-				[1.0, 1.0],
-			],
-			colors: [
-				.cyan.opacity(isAnimating ? 0.2 : 1.0),
-				.cyan,
-				.cyan,
-				.clear,
-				.clear,
-				.clear,
-				.clear,
-				.clear,
-				.clear,
-			],
+			width: width,
+			height: height,
+			points: points,
+			colors: colors,
 			smoothsColors: true
 		)
 		.edgesIgnoringSafeArea(.all)
